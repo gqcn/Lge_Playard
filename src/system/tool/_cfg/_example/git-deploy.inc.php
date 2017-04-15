45b1ec39427514ede406126f756aedcb00bcaef1
John <john@johng.cn>
2017-04-15 23:08:32 +0800

    git自动部署脚本更新

--- src/system/tool/_cfg/_example/git-deploy.inc.php
+++ src/system/tool/_cfg/_example/git-deploy.inc.php
@@ -4,6 +4,6 @@
  */
 return array(
     '配置项名称' => array(
-        array('服务器地址', '默认push分支名称')
+        array('服务器地址', '默认push分支名称', 'ssh push用户对应的服务器密码(非必须)')
     ),
 );
--- src/system/tool/_ctl/GitDeploy.class.php
+++ src/system/tool/_ctl/GitDeploy.class.php
@@ -8,7 +8,8 @@ if(!defined('LGE')){
 /**
  * Git方式自动部署程序到服务器。
  * 使用说明：
- * 1、项目客户端应当已经保存密码，如果是ssh push那么应当保证客户端与服务端已经通过ssh的authorized_keys授权；
+ * 1、项目客户端应当已经保存密码;
+ * 2、如果是ssh push那么应当保证客户端与服务端已经通过ssh的authorized_keys授权，或者，安装sshpass工具，并在配置文件中对服务器指定密码；
  * 2、在项目根目录下执行；
  */
 class Controller_GitDeploy extends Controller_Base
@@ -21,9 +22,14 @@ class Controller_GitDeploy extends Controller_Base
             exit();
         }
         $option       = Lib_ConsoleOption::instance();
-        $deployKey    = $option->getOption('key');
-        $deployBranch = $option->getOption('branch');
-        $deployArray  = Config::get($deployKey, 'git-deploy', true);
+        $deployKey    = $option->getOption('key', 'default');
+        $deployConfig = $option->getOption('config');
+        if (!empty($deployConfig) && file_exists($deployConfig)) {
+            $configArray = include($deployConfig);
+            $deployArray = isset($configArray[$deployKey]) ? $configArray[$deployKey] : array();
+        } else {
+            $deployArray = Config::get($deployKey, 'git-deploy', true);
+        }
         if (empty($deployArray)) {
             echo "找不到\"{$deployKey}\"对应的配置项！\n";
             exit();
@@ -32,8 +38,12 @@ class Controller_GitDeploy extends Controller_Base
             foreach ($deployArray as $k => $item) {
                 $resp   = $item[0];
                 $branch = empty($deployBranch) ? $item[1] : $deployBranch;
-                $cmd    = "git push {$resp} {$branch}";
-                echo ($k + 1).": {$cmd}\n";
+                if (empty($item[2])) {
+                    $cmd = "git push {$resp} {$branch}";
+                } else {
+                    $cmd = "sshpass -p {$item[2]} git push {$resp} {$branch}";
+                }
+                echo ($k + 1).": {$resp} {$branch}\n";
                 exec($cmd);
                 echo "\n";
             }
