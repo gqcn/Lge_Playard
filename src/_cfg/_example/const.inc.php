29e7591874c027a9bc5aed1aad036c56ea11b481
John <john@johng.cn>
2017-04-28 18:25:16 +0800

    代码完善

---
+++ src/_cfg/_example/const.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 一些系统的常量配置（常量是在程序部署时将完全确定的数值，后续运行将不再或者极少进行修改）。
+ *
+ * @author John<john@johnx.cn>
+ */
+
+define('L_DEBUG',                 1);
+define('L_ERROR_LEVEL_FOR_DEBUG', E_ALL);
+define('L_ROOT_PATH',             __DIR__.'/../');
+define('L_DEFAULT_TIME_ZONE',     'Asia/Shanghai');
+define('L_PHAR_FILE_PATH',        '/home/john/Workspace/Lge/dist/lge.phar');
